6342e07e7a7284bd35f24ea32243913d11169a31
jovanlzc <jovan.lazic.10@gmail.com>
2019-04-17 02:45:34 +0200

    Ispravljene metode setPrecnik i setSirina u klasi AutoGuma.

--- VulkanizerskaRadnja/src/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/gume/AutoGuma.java
@@ -51,7 +51,7 @@ public class AutoGuma {
 	 * @throws java.lang.RuntimeException ako je uneti precnik manji od 13 ili veci od 22.
 	 */
 	public void setPrecnik(int precnik){
-		if (precnik < 13 && precnik > 22)
+		if (precnik < 13 || precnik > 22)
 			throw new RuntimeException("Precnik van opsega");
 		this.precnik = precnik;
 	}
@@ -68,7 +68,7 @@ public class AutoGuma {
 	 * @throws java.lang.RuntimeException ako je uneta sirina manja od 135 ili veca od 355.
 	 */
 	public void setSirina(int sirina){
-		if (sirina < 135 && sirina > 355)
+		if (sirina < 135 || sirina > 355)
 			throw new RuntimeException("Sirina van opsega");
 		this.sirina = sirina;
 	}
